f1b5c99e7ff1ef99ad7805f5e393a2e46c0f92c0
sunker0115 <sunker0115@163.com>
2026-06-15 07:24:19 +0800

    fix(frontend): 条件列 120→100

--- frontend/src/pages/rule-editor/DecisionTableEditor.tsx
+++ frontend/src/pages/rule-editor/DecisionTableEditor.tsx
@@ -118,7 +118,7 @@ export default function DecisionTableEditor({
           return (
             <Table.Column
               key={ci}
-              width={145}
+              width={100}
               dataIndex={`_c${ci}`}
               title={(
                 <div style={{
